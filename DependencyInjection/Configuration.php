4d6d5d6124a4d18834cfd50f5040a25fea591d31
Davide Bellettini <davide.bellettini@neomobile.com>
2014-10-17 23:17:04 +0200

    EventStore as Symfony dependency injection service

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,32 @@
+<?php
+namespace EventStore\Bundle\ClientBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('event_store_client');
+
+        $rootNode
+            ->children()
+                ->scalarNode('base_url')
+                    ->cannotBeEmpty()
+                    ->defaultValue('http://127.0.0.1:2113/')
+                ->end()
+            ->end();
+
+        return $treeBuilder;
+    }
+}
---
+++ Tests/DependencyInjection/EventStoreClientExtensionTest.php
@@ -0,0 +1,48 @@
+<?php
+namespace EventStore\Bundle\ClientBundle\Tests\DependencyInjection;
+
+/**
+ * Some methods are from FOSUserBundle (Copyright (c) 2010-2011 FriendsOfSymfony)
+ * @author Davide Bellettini <davide@bellettini.me>>
+ */
+
+use EventStore\Bundle\ClientBundle\DependencyInjection\EventStoreClientExtension;
+use PHPUnit_Framework_TestCase as TestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Yaml\Parser;
+
+class EventStoreClientExtensionTest extends TestCase
+{
+    public function testBaseUrlIsPopulatedCorrectlyByDefault()
+    {
+        $loader = new EventStoreClientExtension();
+        $config = $this->getConfig();
+        unset($config['base_url']);
+
+        $builder = new ContainerBuilder();
+        $loader->load(array($config), $builder);
+
+        $this->assertEquals('http://127.0.0.1:2113/', $builder->getParameter('event_store_client.base_url'));
+
+    }
+
+    public function testBaseUrlIsPopulatedCorrectlyFromConfiguration()
+    {
+        $loader = new EventStoreClientExtension();
+        $config = $this->getConfig();
+
+        $builder = new ContainerBuilder();
+        $loader->load(array($config), $builder);
+
+        $this->assertEquals('http://eventstore-fake.com:2113/', $builder->getParameter('event_store_client.base_url'));
+    }
+
+    private function getConfig()
+    {
+        $yaml = <<<EOF
+base_url: http://eventstore-fake.com:2113/
+EOF;
+
+        return (new Parser())->parse($yaml);
+    }
+}
